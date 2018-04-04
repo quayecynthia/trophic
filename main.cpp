c0ae4ebda0469fd56dccaf341a1df413930cceba
kevinkann98 <kevin.kann@edu.ece.fr>
2018-04-04 11:01:16 +0200

    menu

--- main.cpp
+++ main.cpp
@@ -1,31 +1,46 @@
 #include "grman/grman.h"
 #include <iostream>
-
+#include "menu.h"
 #include "graph.h"
+
 
 int main()
 {
+
+
     /// A appeler en 1er avant d'instancier des objets graphiques etc...
     grman::init();
 
     /// Le nom du répertoire où se trouvent les images à charger
     grman::set_pictures_path("pics");
 
-    /// Un exemple de graphe
+    ///Declaration du menu
+    Menu menu;
+
+    /// Un graphe à charger depuis un fichier choisi
     Graph g;
     g.make_example();
-    g.composantes_connexes();
 
 
-    /// Vous gardez la main sur la "boucle de jeu"
-    /// ( contrairement à des frameworks plus avancés )
-    while ( !key[KEY_ESC] )
+    ///Gestion des menus
+    menu.affiche_principal();
+    menu.gestion_menu2(g);
+
+
+
+    if(menu.getChoix()==2)
     {
-        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
-        g.update();
+        /// Vous gardez la main sur la "boucle de jeu"
+        /// ( contrairement à des frameworks plus avancés )
+        while ( !key[KEY_ESC] )
+        {
 
-        /// Mise à jour générale (clavier/souris/buffer etc...)
-        grman::mettre_a_jour();
+            /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+            g.update();
+
+            /// Mise à jour générale (clavier/souris/buffer etc...)
+            grman::mettre_a_jour();
+        }
     }
 
     grman::fermer_allegro();
